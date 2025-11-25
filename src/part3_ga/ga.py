2d92560fa13f32170e4273e4c8ed8213fcda336f
Samuel Silva Gomes <silvagomes881@gmail.com>
2025-11-24 21:12:17 -0300

    feat: add ga.py

---
+++ src/part3_ga/ga.py
@@ -0,0 +1,168 @@
+import random
+import matplotlib.pyplot as plt
+
+def gerar_itens(numItens, pesoMax, valorMax):
+    """Gera uma lista de itens com base em quantidade, peso máximo e valor máximo."""
+    listaItens = []
+    for _ in range(numItens):
+        pesoItem = random.randint(1,pesoMax)
+        valorItem = random.randint(1,valorMax)
+        item = (pesoItem, valorItem)
+        listaItens.append(item)
+    return listaItens
+
+def gerar_gene():
+    """Gera um gene binário com base no número de itens."""
+    lista = []
+    for _ in range(numItens):
+        num = random.randint(0,1)
+        lista.append(num)
+    
+    return lista
+
+def gerar_lista_genes(tam_populacao):
+    """Gera uma lista de genes binários com base no tamanho da população."""
+    lista_genes = []
+    for _ in range(tam_populacao):
+        gene = gerar_gene()
+        lista_genes.append(gene)
+    return lista_genes
+
+def gerar_fitness(gene, peso_max):
+    """Gera o fitness de um gene com base na soma dos valores de seus itens.
+    O fitness é 0 caso a soma dos pesos dos itens exceda o peso máximo da mochila."""
+    preco = 0
+    peso = 0
+    for i in range(len(gene)):
+        it = gene[i]
+        if it == 1:
+            preco += itens[i][1]
+            peso += itens[i][0]
+
+    if peso > peso_max:
+        preco = 0
+
+    return preco
+
+def gerar_lista_fitness(lista_genes, peso_max):
+    """Gera uma lista com os fitness de todos os genes dentro de uma lista de genes."""
+    lista_fitness = []
+    for gene in lista_genes:
+        fitness = gerar_fitness(gene, peso_max)
+        lista_fitness.append(fitness)
+    return lista_fitness
+
+def porcentagens(lista_fitness):
+    """Gera as porcentagens dos genes com base na lista de fitness correspondente."""
+    lista_porcents = []
+    soma_fitness = sum(lista_fitness)
+    porcentagem = 0
+
+    for i in lista_fitness:
+        porcentagem = i/soma_fitness
+        lista_porcents.append(porcentagem)
+
+    return lista_porcents
+
+def gerar_pai(lista_genes, porcentagensRoleta):
+    """Gera um gene pai aleatório dentro da lista de genes com base no peso da porcentagem."""
+    pai = random.choices(lista_genes, weights=porcentagensRoleta, k=1)
+    pai = pai[0]
+    return pai
+
+def gerar_filhos(pai1, pai2):
+    """Gera dois filhos de dois pais, fazendo o crossover
+    de genes com base em um ponto de corte aleatório e válido."""
+    ponto_corte = random.randint(1, len(pai1)-1)
+
+    pai1_1 = pai1[:ponto_corte]
+    pai1_2 = pai1[ponto_corte:]
+    
+    pai2_1 = pai2[:ponto_corte]
+    pai2_2 = pai2[ponto_corte:]
+
+    filho1 = pai1_1 + pai2_2
+    filho2 = pai2_1 + pai1_2
+
+    return filho1, filho2
+
+def mutacao(lista_fihos, taxaMutacao):
+    """Aplica uma mutação dentro de uma lista de genes
+    com base em uma taxa de mutação."""
+    for filho in lista_fihos:
+        if random.random() < taxaMutacao:
+            i, j = random.sample(range(len(filho)), 2)
+            filho[i], filho[j] = filho[j], filho[i]
+    return lista_filhos
+
+def plot_grafico(lista_f, y_label):
+    """Plota os resultados de uma lista em um gráfico.
+    Aplicado para plotar a média de fitness ou o maior
+    fitness por geração."""
+
+    x = range(len(lista_f))
+    y = lista_f
+    plt.plot(x, y)
+    plt.xlabel('Gerações')
+    plt.ylabel(y_label)
+    plt.title('Resultados do AG para o Knapsack 0/1')
+    plt.grid(True)
+    plt.show()
+
+def print_terminal(i, lista_genes, lista_fitness):
+    print("Geração: ", i)
+    print(lista_genes, "\n")
+    print(lista_fitness, "\n")
+
+if __name__ == '__main__':
+    # Variáveis da geração de itens:
+    numItens       = 10 
+    pesoMax        = 5  
+    valorMax       = 8
+
+    # Limite de peso da mochila:
+    pesoMochila  = 10  
+
+    # Variáveis gerais do algoritmo genético:
+    tamPopulacao = 400
+    numGeracoes  = 1000
+    taxaMutacao  = 0.01
+
+    # Geração de itens e genes iniciais:
+    itens = gerar_itens(numItens, pesoMax, valorMax)
+    print(itens, "\n")
+    lista_genes = gerar_lista_genes(tamPopulacao)
+
+    lista_media_fitness = []
+    lista_maior_fitness = []
+    
+    for i in range(numGeracoes):
+        # Geração dos fitness e suas porcentagens da roleta viciada:
+        lista_fitness       = gerar_lista_fitness(lista_genes, pesoMochila)
+        porcentagensRoleta  = porcentagens(lista_fitness)
+
+        #print_terminal(i, lista_genes, lista_fitness) # Opcional
+
+        # Geração de pais e filhos:
+        lista_filhos = []
+        for _ in range(int((tamPopulacao/2))):
+            pai1 = gerar_pai(lista_genes, porcentagensRoleta)
+            pai2 = gerar_pai(lista_genes, porcentagensRoleta)
+            filho1, filho2 = gerar_filhos(pai1, pai2)
+            lista_filhos.append(filho1)
+            lista_filhos.append(filho2)
+
+        # Obtenção da média de fitness e o maior fitness por geração:
+        media_fitness = sum(lista_fitness)/len(lista_fitness)
+        maior_fitness = max(lista_fitness)
+        lista_media_fitness.append(media_fitness)
+        lista_maior_fitness.append(maior_fitness)
+
+        # Aplicação da mutação na lista de filhos gerados:
+        lista_filhos_mutados = mutacao(lista_filhos, taxaMutacao)
+
+        # Substituição da geração anterior pela atual na lista de genes:
+        lista_genes = lista_filhos_mutados
+    
+    plot_grafico(lista_media_fitness, 'Média do Fitness da Geração')
+    plot_grafico(lista_maior_fitness, 'Maior Fitness da Geração')
